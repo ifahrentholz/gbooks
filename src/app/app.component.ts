6c380b973169f1bd6ffd0393070f116142698361
Ingo Fahrentholz <ingof.work@gmail.com>
2017-03-21 19:47:39 +0100

    Add Material 2 components, add custom theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  spaceScreens: Array<any>;
+
+  constructor(private http: Http) {
+
+  }
+
+  ngOnInit() {
+    this.http.get("./assets/data.json")
+      .map(response => response.json().screenshots)
+      .subscribe(res => this.spaceScreens = res);
+  }
+
+  likeMe(i) {
+    const spaceScreen = this.spaceScreens[i];
+    return spaceScreen.liked = !spaceScreen.liked
+  }
+
+  deleteMe(i) {
+    this.spaceScreens.splice(i, 1);
+    console.log(i);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import 'hammerjs';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MaterialModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -12,9 +14,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
